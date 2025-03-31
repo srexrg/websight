f21941d2ae0dda923874d0a9528a7ae32c0513f7
srexrg <sreeragp276@gmail.com>
2025-03-31 13:20:24 +0530

    update

--- app/(root)/docs/layout.tsx
+++ app/(root)/docs/layout.tsx
@@ -1,11 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export default function DocsLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <div className="text-gray-100 antialiased">
-      {children}
-    </div>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
