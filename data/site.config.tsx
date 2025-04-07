f0fced2305fbf6aa5931ad28654721c48cb20ca2
srexrg <sreeragp276@gmail.com>
2025-04-07 13:00:53 +0530

    improved metadata handling and SEO optimization

---
+++ data/site.config.tsx
@@ -0,0 +1,8 @@
+export const DATA = {
+  name: "WebSight",
+  initials: "websight",
+  url: "https://websight.srexrg.me",
+  prevImage: "/prev.png",
+  description:
+    "WebSight is a powerful opensource alternative to Vercel Analytics.",
+} as const;
